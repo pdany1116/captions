689d26634a4bda6a81e97b74a406b2613d45edbe
Daniel Popa <danypdaniel@gmail.com>
2022-04-30 13:17:00 +0300

    Apply rubocop auto-correct

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class CaptionsController < ApplicationController
   def index
-    head 200
+    head :ok
   end
 end
--- spec/requests/captions_spec.rb
+++ spec/requests/captions_spec.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe "Captions", type: :request do
   describe "GET /captions" do
     it "responds with 200" do
       get "/captions"
-      
-      expect(response).to have_http_status(200)
+
+      expect(response).to have_http_status(:ok)
     end
   end
 end
